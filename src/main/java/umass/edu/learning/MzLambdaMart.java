ff3aed435308fed66333602712ff6e5648598367
luyee <263513621@qq.com>
2016-07-12 02:06:46 +0800

    ranklib to maven first commit

---
+++ src/main/java/umass/edu/learning/MzLambdaMart.java
@@ -0,0 +1,14 @@
+package ciir.umass.edu.learning;
+
+import ciir.umass.edu.learning.tree.LambdaMART;
+
+public class MzLambdaMart {
+	
+//	private 
+//	
+//	public void loadLambdaMartModelFromString(String modelString){
+//		LambdaMART lambdaMart =	new LambdaMART();
+//		lambdaMart.loadModelFromString(modelString);
+//	}
+
+}
